12200e02c632e054ebee3934bd4a00c85400b723
manoel <piloukos45@gmail.com>
2024-12-26 14:01:37 -0300

    fix: test serve

--- src/app/views/Projects/[slug]/page.tsx
+++ src/app/views/Projects/[slug]/page.tsx
@@ -1,33 +1,33 @@
-// ProjectDetails.tsx
-interface ProjectDetailsProps {
-    params: {
-        slug: string;
-    };
-}
+// // ProjectDetails.tsx
+// interface ProjectDetailsProps {
+//     params: {
+//         slug: string;
+//     };
+// }
 
-const ProjectDetails = async ({ params }: ProjectDetailsProps) => {
-    const { slug } = params;
+// const ProjectDetails = async ({ params }: ProjectDetailsProps) => {
+//     const { slug } = params;
 
-    const projectData = await fetchProjectDetails(slug);
+//     const projectData = await fetchProjectDetails(slug);
 
-    return (
-    <section>
-        <h1>Detalhes do Projeto</h1>
-        <p>Slug do projeto: {slug}</p>
-        <p>Dados do projeto: {projectData}</p>
-    </section>
-    );
-};
+//     return (
+//     <section>
+//         <h1>Detalhes do Projeto</h1>
+//         <p>Slug do projeto: {slug}</p>
+//         <p>Dados do projeto: {projectData}</p>
+//     </section>
+//     );
+// };
 
-async function fetchProjectDetails(slug: string) {
-    return `Detalhes do projeto para o slug: ${slug}`;
-}
+// async function fetchProjectDetails(slug: string) {
+//     return `Detalhes do projeto para o slug: ${slug}`;
+// }
 
-export async function generateStaticParams() {
-    return [
-    { slug: 'projeto-1' },
-    { slug: 'projeto-2' },
-    ];
-}
+// export async function generateStaticParams() {
+//     return [
+//     { slug: 'projeto-1' },
+//     { slug: 'projeto-2' },
+//     ];
+// }
 
-export default ProjectDetails;
+// export default ProjectDetails;
